3093c5b49aeee15367e820129ac069f7e90c7002
Maksim Gumerov <maxim.gumerov@massmo.ru>
2016-05-08 14:07:01 +0500

    Reverted commits involving Razor To push data from http POST request into the view, we can use native Angular means, there is no need for server-side page preprocessing.

--- webserver/Program.cs
+++ webserver/Program.cs
@@ -7,7 +7,9 @@ using System.Windows.Forms;
 using Nancy.Hosting.Self;
 using Nancy;
 using Nancy.Conventions;
-using Nancy.ViewEngines.Razor;
+using Nancy.ModelBinding;
+using System.IO;
+using Nancy.Responses;
 
 namespace webserver
 {
@@ -18,7 +20,7 @@ namespace webserver
         {
             HostConfiguration cfg = new HostConfiguration();
             cfg.RewriteLocalhost = false;
-            using (var host = new NancyHost(new ApplicationBootstrapper(/*args.Length == 1 ? args[0] : */AppDomain.CurrentDomain.BaseDirectory),
+            using (var host = new NancyHost(new ApplicationBootstrapper(/*args.Length == 1 ? args[0] :*/ AppDomain.CurrentDomain.BaseDirectory),
                 cfg, new Uri("http://localhost:8080")))
             {
                 host.Start();
@@ -38,28 +40,40 @@ namespace webserver
         public OilBalancesParams oilBalances;
         public float waterCompressibility;
     }
-    /*
-    public class RazorConfig : IRazorConfiguration
+
+    public class Sucaba : NancyModule
     {
-        public IEnumerable<string> GetAssemblyNames()
+        public Sucaba(IRootPathProvider pathProvider)
         {
-            yield return "HyRes.Models";
-            yield return "HyRes.Website";
-        }
+            Post["/chart"] = x =>
+            {
+                //Вытащим параметры расчета из запроса. Но использовать не будем, отдадим муляж.
+                CalcParams calcparams = this.Bind<CalcParams>();
 
-        public IEnumerable<string> GetDefaultNamespaces()
-        {
-            yield return "HyRes.Models";
-            yield return "HyRes.Website.Infrastructure.Helpers";
-        }
+                var ss = new MemoryStream();
+                StreamWriter sw = new StreamWriter(ss);
+                sw.WriteLine("Date,V1,V2");
+                DateTime dt = DateTime.Today;
+                Random r = new Random();
+                for (int i = 0; i < 100; i++)
+                {
+                    sw.Write(string.Format("{0:yyyy}{0:MM}{0:dd}", dt.AddDays(i)));
+                    sw.Write(",");
+                    sw.Write(r.Next(30));
+                    sw.Write(",");
+                    sw.Write(r.Next(30));
+                    sw.WriteLine();
+                }
+                sw.Flush();
+                ss.Position = 0;
 
-        public bool AutoIncludeModelNamespace
-        {
-            get { return true; }
+                var response = new StreamResponse(() => ss, MimeTypes.GetMimeType("text/csv"));
+                  
+                return response.AsAttachment("chart.csv");
+            };
         }
     }
-     * */
-
+    
     public class ApplicationBootstrapper : DefaultNancyBootstrapper
     {
         private readonly IRootPathProvider rootPathProvider;
--- webserver/Sucaba.cs
+++ webserver/Sucaba.cs
@@ -3,47 +3,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.IO;
-using Nancy.Responses;
-using Nancy.ModelBinding;
 
 namespace webserver
 {
-    public class Sucaba : NancyModule
+  /*  class Sucaba : NancyModule
     {
-        public Sucaba(IRootPathProvider pathProvider)
+        public Sucaba()
         {
-            Post["/chart"] = x =>
-            {
-                //Вытащим параметры расчета из запроса. Но использовать не будем, отдадим муляж.
-                CalcParams calcparams = this.Bind<CalcParams>();
-
-                var ss = new MemoryStream();
-                StreamWriter sw = new StreamWriter(ss);
-                sw.WriteLine("Date,V1,V2");
-                DateTime dt = DateTime.Today;
-                Random r = new Random();
-                for (int i = 0; i < 100; i++)
-                {
-                    sw.Write(string.Format("{0:yyyy}{0:MM}{0:dd}", dt.AddDays(i)));
-                    sw.Write(",");
-                    sw.Write(r.Next(30));
-                    sw.Write(",");
-                    sw.Write(r.Next(30));
-                    sw.WriteLine();
-                }
-                sw.Flush();
-                ss.Position = 0;
-
-                var response = new StreamResponse(() => ss, MimeTypes.GetMimeType("text/csv"));
-                  
-                return response.AsAttachment("chart.csv");
-            };
-
             Get["/"] = x =>
             {
-                return View["form", null /*CalcParams*/];
+                return Response.AsFile("static\temperatures.csv");
             };
         }
-    }
+    }*/
 }
